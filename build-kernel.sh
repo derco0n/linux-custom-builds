6468b26a2ba6ac233bf0016a14ca796fa05e779f
derco0n <dennis_marx@web.de>
2019-02-28 20:54:27 +0100

    initial commit

--- build-kernel.sh
+++ build-kernel.sh
@@ -3,14 +3,14 @@ BUILD_START=$(date +"%s")
 
 export CONCURRENCY_LEVEL=2 
 export CHOST="x86_64-pc-Linux-gnu" 
-export CFLAGS="-march=native -O2 -j3 -pipe" 
+export CFLAGS="-march=native -O2 -pipe" 
 export CXXFLAGS="$CFLAGS"
 
 echo "Start Kernel build..."
 echo "#####################"
 echo ""
-fakeroot make-kpkg  --initrd --append-to-version=-coonzmod-eeepc kernel_image kernel_headers
+fakeroot make-kpkg  --initrd -j3 --append-to-version=-coonzmod-eeepc kernel_image kernel_headers
 
 BUILD_END=$(date +"%s")
 DIFF=$(($BUILD_END - $BUILD_START))
-echo -e "$Yellow Build completed in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds.$nocol"
+echo -e "$Yellow Build ended after $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds.$nocol"
