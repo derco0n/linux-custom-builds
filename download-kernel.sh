4832d38bb6e028d32c936d0cfc7830f97240ed00
Dennis Marx <dennis_marx@web.de>
2019-06-04 21:08:31 +0200

    Update download-kernel.sh

--- download-kernel.sh
+++ download-kernel.sh
@@ -3,22 +3,24 @@
 #DOWNLOADPATH="https://mirrors.edge.kernel.org/pub/linux/kernel/v4.x/"
 #KERNEL="linux-4.20.9.tar.xz"  # Change Archive as needed...
 
-DOWNLOADPATH="https://mirrors.edge.kernel.org/pub/linux/kernel/v5.x/"
-KERNEL="linux-5.1.7.tar.xz"  # Change Archive as needed...
+DOWNLOADPATH="https://github.com/multipath-tcp/mptcp_net-next/archive/"
+KERNEL="export.zip"  # Change Archive as needed...
+
+KERNELDIR="Linux-Upstream-MPTCP"
 
 wget $DOWNLOADPATH$KERNEL
 echo Unpacking $KERNEL
-tar -Jxf $KERNEL
+unzip $KERNEL -d ./$KERNELDIR
 
-echo "Kernel: $KERNEL"
+#echo "Kernel: $KERNEL"
+echo "Kernel: $KERNELDIR"
 
-KDIR1=`echo "$KERNEL" | cut -f 1 -d '.'`
-KDIR2=`echo "$KERNEL" | cut -f 2 -d '.'`
-KDIR3=`echo "$KERNEL" | cut -f 3 -d '.'`
+#KDIR1=`echo "$KERNEL" | cut -f 1 -d '.'`
+#KDIR2=`echo "$KERNEL" | cut -f 2 -d '.'`
+#KDIR3=`echo "$KERNEL" | cut -f 3 -d '.'`
 
-KERNELDIR="$KDIR1.$KDIR2.$KDIR3"
+#KERNELDIR="$KDIR1.$KDIR2.$KDIR3"
 #KERNELDIR="$KDIR1.$KDIR2"
-
 
 echo "Kerneldir: $KERNELDIR ..."
 
