e6a843a1823da19557b82f44132f8584452359c3
derco0n <dennis_marx@web.de>
2019-08-24 16:50:20 +0200

    added-config to repo

--- build-kernel.sh
+++ build-kernel.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 BUILD_START=$(date +"%s")
 
-THREADS=3
+THREADS=1
 
 export CHOST="x86_64-pc-Linux-gnu" #https://wiki.gentoo.org/wiki/CHOST #Targetarchitecture
-export CFLAGS="-march=native -O2 -pipe" #https://wiki.gentoo.org/wiki/Safe_CFLAGS#Intel
+#export CFLAGS="-march=native -O2 -pipe" #https://wiki.gentoo.org/wiki/Safe_CFLAGS#Intel
+export CFLAGS="-march=atom -mtune=atom -O3 -pipe" #GCC4.5 contains tuning flags for Intel Atom. O3 optimized for speed
 export CXXFLAGS="$CFLAGS"
 
 
